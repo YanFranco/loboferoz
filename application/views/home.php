356e318a1381490f03e650934c94537a4e429725
Miguel Velasquez <mvelasquezp88@gmail.com>
2014-06-09 16:52:34 -0500

    Actualizacion de la vista de prueba

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,10 @@
 <html>
 	<head>
 		<meta charset="UTF-8" >
-		<title>Portada de mi sitio</title>
+		<title>Administración del conocimiento</title>
 	</head>
 	<body>
-		<h1>Bienvenido a mi web</h1>
-		<p>Esta es la portada de página web, basada en la publicación de artículos interesantes.</p>
+		<h1>Lobo Feroz</h1>
+		<p>Esta es una página de prueba.</p>
 	</body>
 </html> 
